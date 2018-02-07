7bf361ab402b8503eff06e1531b9d9e89dd0bde8
gmakersteam <gmakers.team@gmail.com>
2018-02-07 13:30:02 -0500

    function showPokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 'use stric';
 const id = '001';
-
+pokemon(id);
 async function pokemon (id) {
-    let pokemonData = await fetch('data/pokemon-list.json');
+    let pokemonData = await fetch('https://raw.githubusercontent.com/Mariley20/reto1-web/master/data/pokemon-list.json');
     let pokemonJSON = await pokemonData.json();
     console.log('data', pokemonJSON);
+    showPokemon(pokemonJSON);
   }
-  pokemon(id);
+function showPokemon(data) {
+    
+}
